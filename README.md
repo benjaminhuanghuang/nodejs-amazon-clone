1e0e81e19827331c997cbb326f6e24f2ddc108b9
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-05-08 00:27:40 -0700

    module and db connect

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
     Morgan (logger middleware)
     EJS    
     MongoDB on mongoLab
+    Mongoose (Mongo ORM)
     Elastic search
     JQuery
     Stripe 
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var morgan = require("morgan");
-var app = express();
+var mongoose = require('mongoose');
 
+
+var app = express();
+mongoose.connect('mongodb://root:123abc@ds133961.mlab.com:33961/ecomm', function(err){
+    if (err){
+        console.log(err);
+    }
+    else
+    {
+        console.log("Connected...");
+    }
+});
 // Using middleware
 app.use(morgan('dev'));
 
